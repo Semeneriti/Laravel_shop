ddf5a3ced8a002ea25136d470e8757f13aca5a6f
Semeneriti <buhtabest@gmail.com>
2026-06-27 11:47:38 +0500

    Revert "Fortify + Breeze"
    This reverts commit cc0c0641e21f60d38c14f69d3980c73ddb7b753d.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register.form');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/login', function () {
+    return 'Страница входа (будет позже)';
+})->name('login.form');
